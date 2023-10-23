384efed63296e82d72281063cf31a96dce4d239a
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-23 15:07:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+
+	
 function insert_Row() {
     //Write your code here
-  
+    const table =document.getElementById("sampleTable");
+    const a=table.childNodes;
+    const tr=document.createElement("tr");
+    tr.innerHTML=`<td>New cell1</td> 
+    <td>New cell2</td>`
+    table.insertBefore(tr,a[1]);
   
 }
